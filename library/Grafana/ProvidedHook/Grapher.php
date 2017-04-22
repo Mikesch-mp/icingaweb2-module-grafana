Mikesch-mp/icingaweb2-module-grafana

commit 70a61a3b37965aefcfe70d5df8be25ab43aa2deb

Access mode can be proxy or direct now

library/Grafana/ProvidedHook/Grapher.php
@@ -29,6 +29,7 @@ class Grapher extends GrapherHook
     protected $defaultDashboard = "icinga2-default";
     protected $defaultDashboardStore = "db";
     protected $datasource = null;
+    protected $accessmode = "proxy";
     protected $timeranges = [
                           '5m'   => '5 minutes',
                           '15m'  => '15 minutes',
@@ -77,7 +78,7 @@ class Grapher extends GrapherHook
         $this->defaultDashboard = $this->config->get('defaultdashboard', $this->defaultDashboard);
         $this->defaultDashboardStore = $this->config->get('defaultdashboardstore', $this->defaultDashboardStore);
 	$this->datasource = $this->config->get('datasource', $this->datasource);
-        $this->view = Icinga::app()->getViewRenderer()->view;
+        $this->accessmode = $this->config->get('accessmode', $this->accessmode);
         if($this->username != null)
         {
             if($this->password != null)
@@ -127,6 +128,7 @@ class Grapher extends GrapherHook
 
     private function getTimerangeLink($object, $rangeName, $timeRange)
     {
+        $this->view = Icinga::app()->getViewRenderer()->view;
         if ($object instanceof Host)
         {
 	    $array = [
@@ -157,10 +159,10 @@ class Grapher extends GrapherHook
 
     private function getPreviewImage($serviceName, $hostName)
     {
-	$pngUrl = sprintf(
-			'%s://%s%s/render/dashboard-solo/%s/%s?var-hostname=%s&var-service=%s%s&panelId=%s&width=%s&height=%s&theme=light&from=now-%s&to=now',
+        if ($this->accessmode == "proxy") {
+	    $pngUrl = sprintf(
+			'%s://%s/render/dashboard-solo/%s/%s?var-hostname=%s&var-service=%s%s&panelId=%s&width=%s&height=%s&theme=light&from=now-%s&to=now',
 			$this->protocol,
-			$this->auth,
 			$this->grafanaHost,
 			$this->dashboardstore,
 			$this->dashboard,
@@ -171,32 +173,54 @@ class Grapher extends GrapherHook
 			$this->width,
 			$this->height,
 			$this->timerange
-	);
+	    );
 
-        // fetch image with curl
-        $curl_handle = curl_init();
-        curl_setopt($curl_handle,CURLOPT_URL,$pngUrl);
-        curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,2);
-        curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,true);
-        curl_setopt($curl_handle,CURLOPT_SSL_VERIFYPEER,false);
-        curl_setopt($curl_handle,CURLOPT_TIMEOUT,5);
-        $imgBinary = curl_exec($curl_handle);
-        if(curl_error($curl_handle))
-        {
-            return 'Graph currently unavailable: :' . curl_error($curl_handle);
+            // fetch image with curl
+            $curl_handle = curl_init();
+            curl_setopt($curl_handle,CURLOPT_URL,$pngUrl);
+            curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,2);
+            curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,true);
+            curl_setopt($curl_handle,CURLOPT_SSL_VERIFYPEER,false);
+            curl_setopt($curl_handle,CURLOPT_TIMEOUT,5);
+            curl_setopt($curl_handle, CURLOPT_USERPWD, "$this->auth");
+	    curl_setopt($curl_handle, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+            $imgBinary = curl_exec($curl_handle);
+            if(curl_error($curl_handle))
+            {
+                return 'Graph currently unavailable: :' . curl_error($curl_handle);
+            }
+            curl_close($curl_handle);
+
+
+            $img = 'data:image/png;base64,'.base64_encode($imgBinary);
+            $imghtml = '<img src="%s" alt="%s" width="%d" height="%d" />';
+            return sprintf(
+                $imghtml,
+                $img,
+                rawurlencode($serviceName),
+                $this->width,
+                $this->height
+            );
+        } else {
+            $imghtml = '<img src="%s://%s/render/dashboard-solo/%s/%s?var-hostname=%s&var-service=%s%s&panelId=%s&width=%s&height=%s&theme=light&from=now-%s&to=now" alt="%s" width="%d" height="%d" />';
+            return sprintf(
+			$imghtml,
+                        $this->protocol,
+                        $this->grafanaHost,
+                        $this->dashboardstore,
+                        $this->dashboard,
+                        urlencode($hostName),
+                        rawurlencode($serviceName),
+                        $this->customVars,
+                        $this->panelId,
+                        $this->width,
+                        $this->height,
+                        $this->timerange,
+                        rawurlencode($serviceName),
+                        $this->width,
+                        $this->height
+            );
         }
-        curl_close($curl_handle);
-
-
-        $img = 'data:image/png;base64,'.base64_encode($imgBinary);
-        $imghtml = '<img src="%s" alt="%s" width="%d" height="%d" />';
-        return sprintf(
-            $imghtml,
-            $img,
-            rawurlencode($serviceName),
-            $this->width,
-            $this->height
-      );
     }
 
     public function has(MonitoredObject $object)

application/forms/Config/GeneralConfigForm.php
@@ -151,7 +151,18 @@ class GeneralConfigForm extends ConfigForm
                 'description'           => $this->translate('Grafana Datasource Type.')
             )
         );
-
+        $this->addElement(
+            'select',
+            'grafana_accessmode',
+            array(
+                'label'                 => $this->translate('Grafana access'),
+                'multiOptions' => array(
+                    'direct'                => $this->translate('Direct'),
+                    'proxy'                 => $this->translate('Proxy'),
+                ),
+                'description'           => $this->translate('User access Grafana directly or module proxies graphs.')
+            )
+        );
     }
 }
 

module.info
@@ -1,5 +1,5 @@
 Name: Grafana
-Version: 1.0.11
+Version: 1.1.0
 Depends: monitoring
 Description: Grafana - A perfdata visualisation module
  Shows Grafana graphs for captured metrics.
@@ -9,6 +9,8 @@ Description: Grafana - A perfdata visualisation module
  * Grafana (>= 4.1.0) with basic auth enabled or anonymous access
 
  * InfluxDB or Graphite as datasource backend.
+
+ * PHP with curl enabled (for proxy mode)
 
 
  Thanks for bug repoting/fixing or enhancements goes to:
@@ -20,3 +22,5 @@ Description: Grafana - A perfdata visualisation module
  Peter N.
 
  Alex
+
+ Michael F.

commit 07afe2131a7f5777a32ebbab40408848874645e9

Better error handling for proxy access mode
- use curl_set_opts
- do not initialize $this->auth with an `@` at the end, but pass this directly to curl_opts (bug introduced in 70a61a3b37965aefcfe70d5df8be25ab43aa2deb)
- curl_error($curl_handle) is not a safe error handling when RETURNTRANSFER is set to true (must check for $res === false, http://stackoverflow.com/questions/16452636/php-curl-what-does-curl-exec-return)
- fetch the http status code and return an error
- add Util class with http status code translation to string
- better explanation of `accesscode` in the docs

fixes #31
fixes #32

library/Grafana/ProvidedHook/Grapher.php
@@ -5,12 +5,14 @@ namespace Icinga\Module\Grafana\ProvidedHook;
 use Icinga\Application\Icinga;
 use Icinga\Application\Config;
 use Icinga\Exception\ConfigurationError;
+use Exception;
 use Icinga\Application\Hook\GrapherHook;
 use Icinga\Module\Monitoring\Object\MonitoredObject;
 use Icinga\Module\Monitoring\Object\Host;
 use Icinga\Module\Monitoring\Object\Service;
 use Icinga\Web\Url;
 use Icinga\Web\View;
+use Icinga\Module\Grafana\Util;
 
 class Grapher extends GrapherHook
 {
@@ -83,12 +85,12 @@ class Grapher extends GrapherHook
         {
             if($this->password != null)
             {
-                $this->auth = $this->username.":".$this->password."@";
+                $this->auth = $this->username.":".$this->password;
             }
             else
-           {
-                $this->auth = $this->username."@";
-           }
+            {
+                $this->auth = $this->username;
+            }
         }
         else
         {
@@ -177,22 +179,35 @@ class Grapher extends GrapherHook
 
             // fetch image with curl
             $curl_handle = curl_init();
-            curl_setopt($curl_handle,CURLOPT_URL,$pngUrl);
-            curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,2);
-            curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,true);
-            curl_setopt($curl_handle,CURLOPT_SSL_VERIFYPEER,false);
-            curl_setopt($curl_handle,CURLOPT_TIMEOUT,5);
-            curl_setopt($curl_handle, CURLOPT_USERPWD, "$this->auth");
-	    curl_setopt($curl_handle, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
-            $imgBinary = curl_exec($curl_handle);
-            if(curl_error($curl_handle))
-            {
-                return 'Graph currently unavailable: :' . curl_error($curl_handle);
+            $curl_opts = array(
+                CURLOPT_URL => $pngUrl,
+                CURLOPT_CONNECTTIMEOUT => 2,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_SSL_VERIFYPEER => false, //TODO: config option
+                CURLOPT_TIMEOUT => 5, //TODO: config option
+                CURLOPT_USERPWD => "$this->auth",
+                CURLOPT_HTTPAUTH, CURLAUTH_ANY
+            );
+
+            curl_setopt_array($curl_handle, $curl_opts);
+
+            $res = curl_exec($curl_handle);
+
+            if ($res === false) {
+                return "<b>Graph currently unavailable: Curl error: ' . curl_error($curl_handle)</b>";
             }
+
+            $statusCode = curl_getinfo($curl_handle, CURLINFO_HTTP_CODE);
+
+            if ($statusCode > 299) {
+                $error = @json_decode($res);
+		return "<b>Cannot fetch Grafana graph: ". Util::httpStatusCodeToString($statusCode) .
+                       " ($statusCode)</b>: " . (property_exists($error, 'message') ? $error->message : "");
+            }
+
             curl_close($curl_handle);
 
-
-            $img = 'data:image/png;base64,'.base64_encode($imgBinary);
+            $img = 'data:image/png;base64,'.base64_encode($res);
             $imghtml = '<img src="%s" alt="%s" width="%d" height="%d" />';
             return sprintf(
                 $imghtml,

library/Grafana/Util.php
@@ -1,0 +1,59 @@
+<?php
+
+namespace Icinga\Module\Grafana;
+
+class Util
+{
+
+    public static function httpStatusCodetoString($code = 0)
+    {
+        $statuscodes = array(
+            '100' => 'Continue',
+            '101' => 'Switching Protocols',
+            '200' => 'OK',
+            '201' => 'Created',
+            '202' => 'Accepted',
+            '203' => 'Non-Authoritative Information',
+            '204' => 'No Content',
+            '205' => 'Reset Content',
+            '206' => 'Partial Content',
+            '300' => 'Multiple Choices',
+            '302' => 'Found',
+            '303' => 'See Other',
+            '304' => 'Not Modified',
+            '305' => 'Use Proxy',
+            '400' => 'Bad Request',
+            '401' => 'Unauthorized',
+            '402' => 'Payment Required',
+            '403' => 'Forbidden',
+            '404' => 'Not Found',
+            '405' => 'Method Not Allowed',
+            '406' => 'Not Acceptable',
+            '407' => 'Proxy Authentication Required',
+            '408' => 'Request Timeout',
+            '409' => 'Conflict',
+            '410' => 'Gone',
+            '411' => 'Length Required',
+            '412' => 'Precondition Failed',
+            '413' => 'Request Entity Too Large',
+            '414' => 'Request-URI Too Long',
+            '415' => 'Unsupported Media Type',
+            '416' => 'Requested Range Not Satisfiable',
+            '417' => 'Expectation Failed',
+            '500' => 'Internal Server Error',
+            '501' => 'Not Implemented',
+            '502' => 'Bad Gateway',
+            '503' => 'Service Unavailable',
+            '504' => 'Gateway Timeout',
+            '505' => 'HTTP Version Not Supported'
+        );
+        
+        $code = (string)$code;
+        
+        if(array_key_exists($code, $statuscodes)) {
+            return $statuscodes[$code];
+        } else {
+            return $code;
+        }
+    }
+}