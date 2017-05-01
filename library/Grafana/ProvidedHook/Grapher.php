5ee63927e313486dd2d34e0d3a636d8104a9ae41
Carsten <carsten.koebke@gmx.de>
2017-05-01 22:53:03 +0200

    Update Grapher.php
    Fix typo from #43

--- library/Grafana/ProvidedHook/Grapher.php
+++ library/Grafana/ProvidedHook/Grapher.php
@@ -189,7 +189,7 @@ class Grapher extends GrapherHook
                 CURLOPT_CONNECTTIMEOUT => 2,
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_SSL_VERIFYPEER => false, //TODO: config option
-                CURLOPT_TIMEOUT => $this-timeout, 
+                CURLOPT_TIMEOUT => $this->timeout, 
                 CURLOPT_USERPWD => "$this->auth",
                 CURLOPT_HTTPAUTH, CURLAUTH_ANY
             );
