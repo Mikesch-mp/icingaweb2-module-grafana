b86ef667d1ee49ee330486e94e1cf09c97d55a7e
Carsten Koebke <carsten.koebke@koebbes.de>
2018-08-04 12:47:12 +0200

    Fix for disabled graphs on show all page. Fixes #172

--- application/controllers/ShowController.php
+++ application/controllers/ShowController.php
@@ -14,6 +14,7 @@ use Icinga\Module\Monitoring\Object\Host;
 use Icinga\Web\Widget\Tab;
 use Icinga\Web\Hook;
 use Icinga\Module\Grafana\Helpers\Timeranges;
+use Icinga\Application\Config;
 
 
 class ShowController extends MonitoringAwareController
@@ -21,6 +22,7 @@ class ShowController extends MonitoringAwareController
     /** @var bool */
     protected $showFullscreen;
     protected $host;
+    protected $custvardisable = "grafana_graph_disable";
 
     public function init()
     {
@@ -29,6 +31,11 @@ class ShowController extends MonitoringAwareController
             = $this->showFullscreen
             = (bool)$this->_helper->layout()->showFullscreen;
         $this->host = $this->getParam('host');
+        $this->config = Config::module('grafana')->getSection('grafana');
+        /**
+         * Name of the custom varibale to disable graph
+         */
+        $this->custvardisable = ($this->config->get('custvardisable', $this->custvardisable));
     }
 
     public function indexAction()
@@ -68,8 +75,11 @@ class ShowController extends MonitoringAwareController
         $this->view->menu = $menu->getTimerangeMenu();
 
         /* first host object for host graph */
-        $objects[] = $this->getHostObject($this->host);
-
+        $this->object = $this->getHostObject($this->host);
+        $customvars= $this->object->fetchCustomvars()->customvars;
+        if ($this->object->process_perfdata || !(isset($customvars[$this->custvardisable]) && json_decode(strtolower($customvars[$this->custvardisable])) !== false)) {
+            $objects[] = $this->object;
+        }
         /* Get all services for this host */
         $query = $this->backend->select()->from('servicestatus', [
             'service_description',
@@ -77,7 +87,11 @@ class ShowController extends MonitoringAwareController
         $this->applyRestriction('monitoring/filter/objects', $query);
 
         foreach ($query->where('host_name', $this->host) as $service) {
-            $objects[] = $this->getServiceObject($service->service_description, $this->host);
+            $this->object = $this->getServiceObject($service->service_description, $this->host);
+            $customvars= $this->object->fetchCustomvars()->customvars;
+            if ($this->object->process_perfdata && !(isset($customvars[$this->custvardisable]) && json_decode(strtolower($customvars[$this->custvardisable])) !== false)) {
+                $objects[] = $this->object;
+            }
         }
         $this->view->objects = $objects;
         $this->view->grapher = Hook::first('grapher');
