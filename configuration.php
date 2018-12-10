77f24fcebe1aa5da548fbafbc78a70756919b5ee
ckoebke <Alph-10.110.0.6>
2018-12-10 17:42:34 +0100

    Add missing permission

--- configuration.php
+++ configuration.php
@@ -4,6 +4,7 @@ use Icinga\Authentication\Auth;
 $auth = Auth::getInstance();
 
 $this->providePermission('grafana/graphconfig', $this->translate('Allow to configure graphs.'));
+$this->providePermission('grafana/graph', $this->translate('Allow to view graphs in dashboards.'));
 $this->providePermission('grafana/debug', $this->translate('Allow to see module debug informations.'));
 $this->providePermission('grafana/showall', $this->translate('Allow access to see all graphs of a host.'));
 $this->providePermission('grafana/enablelink', $this->translate('Allow to follow links to Grafana'));
